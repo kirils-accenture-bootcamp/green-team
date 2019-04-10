d36edee0709962a7aaf3f7e18e8694ae2527feeb
Student <student>
2019-04-10 12:14:56 +0300

    initial

---
+++ src/test/java/stepDefinitions/Example.java
@@ -0,0 +1,12 @@
+package stepDefinitions;
+
+import org.openqa.selenium.WebDriver;
+
+public class Example {
+    private WebDriver driver;
+
+    public Example() {
+        this.driver = Hooks.driver;
+    }
+
+}
